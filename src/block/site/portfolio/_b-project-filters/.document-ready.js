d0b60cb14afd1563ec188854a571dcf342415cbe
Alexander <alexander.zybin@gmail.com>
2016-06-30 23:08:41 +0300

    201606302308

--- src/block/site/portfolio/_b-project-filters/.document-ready.js
+++ src/block/site/portfolio/_b-project-filters/.document-ready.js
@@ -5,9 +5,17 @@ $(function(){
 		event.preventDefault();
 		
 		var btn = $(this);
+		var cat_flt = btn.attr('data-cat-flt') || 0;
 		
 		$('.b-project-filters .type-filters a').removeClass('active');
 		btn.addClass('active');
+		
+		$('.b-project-list .project-cont').hide();
+		if(cat_flt) {
+			$('.b-project-list .project-cont[data-cat-flt="' + cat_flt + '"]').fadeIn('fast');
+		} else {
+			$('.b-project-list .project-cont').fadeIn('fast');
+		}
 		
 	});
 	
